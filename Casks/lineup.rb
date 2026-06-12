124168dca2cb602a50842a16cf26a76f26178640
Jean Humann <jeanhumann@gmail.com>
2026-06-12 17:17:35 +0200

    cask: drop redundant verified: param (same domain as homepage)

--- Casks/lineup.rb
+++ Casks/lineup.rb
@@ -2,8 +2,7 @@ cask "lineup" do
   version "1.0.1"
   sha256 "527a0bf5e5e130275bccf2f806c60e8d207f59c5da171a42c003ee89b4cdc8b4"
 
-  url "https://github.com/cleyrop/Lineup/releases/download/v#{version}/Lineup-#{version}.dmg",
-      verified: "github.com/cleyrop/Lineup/"
+  url "https://github.com/cleyrop/Lineup/releases/download/v#{version}/Lineup-#{version}.dmg"
   name "Lineup"
   desc "Focused, list-only macOS window switcher"
   homepage "https://github.com/cleyrop/Lineup"
